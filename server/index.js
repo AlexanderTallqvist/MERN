5d63c5b1b60166e19da622bf5aa4261655695152
Alexander Tallqvsit <alexander_tallqvist@hotmail.com>
2018-01-06 13:21:01 +0200

    Testing stuff

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const app = express();
 // Tell passport to use our GoogleStrategy
 passport.use(new GoogleStrategy());
 
+app.get("/", (req, res) => {
+  res.send({"Hello": "World"});
+});
+
 // Port setup
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("SERVER UP ON PORT: " + PORT));
